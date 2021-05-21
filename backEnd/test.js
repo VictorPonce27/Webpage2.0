4445d1622f329d3d49fee4b8b816e857e7bb84a2
VictorP <A00827302@itesm.mx>
2021-05-21 11:59:07 -0500

    working on the backend

--- backEnd/test.js
+++ backEnd/test.js
@@ -1,20 +1,19 @@
 const express = require('express'); 
 var request = require('request'); 
 const app = express(); 
-const port = 4001; 
+const port = 4015; 
+const sqlConfig = require('./sqlConfig'); 
+
 
 app.get('/', function (req, res) {
     var sql = require("mssql");
-
-   
-
 
     sql.connect(sqlConfig, function (err) {
         if (err) console.log(err);
 
         var request = new sql.Request();
 
-        request.query('select * from empleado', function (err, recordset) {
+        request.query("select * from empleado where empleado.id = '557058:0444128a-ce7b-46a2-a3b9'", function (err, recordset) {
             if (err) console.log(err);
             res.send(recordset);
         });
