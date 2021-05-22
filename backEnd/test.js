77ca0427bd904390947ed4529292e2816bdf10c1
VictorP <A00827302@itesm.mx>
2021-05-22 13:18:38 -0500

    working on setting and getting values

--- backEnd/test.js
+++ backEnd/test.js
@@ -1,24 +1,55 @@
-const express = require('express'); 
-var request = require('request'); 
-const app = express(); 
-const port = 4015; 
-const sqlConfig = require('./sqlConfig'); 
+const express = require('express');
+var request = require('request');
+const app = express();
+const port = process.env.PORT || 5000;
+const sqlConfig = require('./sqlConfig');
 
 
-app.get('/', function (req, res) {
-    var sql = require("mssql");
+// app.get('/', function (req, res) {
+//     var sql = require("mssql");
 
-    sql.connect(sqlConfig, function (err) {
+//     sql.connect(sqlConfig, function (err) {
+//         if (err) console.log(err);
+
+//         var request = new sql.Request();
+
+//         request.query("select * from empleado where empleado.id = '557058:0444128a-ce7b-46a2-a3b9'", function (err, recordset) {
+//             if (err) console.log(err);
+//             res.send(recordset);
+//         });
+//     });
+// });
+
+app.use("/", express.static("public"));
+app.use(express.json());
+
+app.post("/login", (req, res) => {
+    const conn = sql.connect(sqlConfig, (err) => {
         if (err) console.log(err);
-
-        var request = new sql.Request();
-
-        request.query("select * from empleado where empleado.id = '557058:0444128a-ce7b-46a2-a3b9'", function (err, recordset) {
+        const request = new sql.Request();
+        let query =
+            "select * from empleado where UserName='" + req.body.UserName + "'";
+        console.log(query);
+        request.query(query, (err, { recordset }) => {
             if (err) console.log(err);
-            res.send(recordset);
+            console.log(recordset[0]);
+            let login = false;
+            if (
+                recordset[0].UserName === req.body.UserName
+            ) {
+                login = true;
+            }
+            res.json({
+                login: login,
+                resultado: recordset[0],
+            });
         });
     });
+    console.log("Post Login");
+    console.log(req.body);
 });
+
+
 
 var server = app.listen({ port }, function () {
     console.log('Server is running..');
--- frontEnd/src/Pages/Welcome.js
+++ frontEnd/src/Pages/Welcome.js
@@ -1,30 +1,60 @@
+import React from 'react';
 import '../CSS/App.css';
-import Button from '@material-ui/core/Button'; 
-import ButtonGroup from '@material-ui/core/ButtonGroup'; 
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+// TODO: Fix the error of missign "{" check this page for more info https://www.pluralsight.com/guides/extracting-input-from-a-form-with-reactjs
 
-function Welcome() {
-  return (
-    <div>
-      <div className='box'>
-        <form action="">
-          <span className="text-center">Welcome to Valgrind</span>
-          <div className="input-container">
-            <input type="email" required="required" placeholder="User@gmail.com" href='/UserProfile' />
-            <label>User:
+
+class Welcome extends React.Component {
+
+  handleNameChange(event) { 
+    var user = this.state.user; 
+    user.name = event.target.value; 
+    this.setState({user:user}); 
+  }
+
+  handleButtonClicked(){
+    console.log(this.state.user); 
+    alert(this.state.user); 
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = { 
+      user: {
+        name: props.name
+      }
+    }
+  }
+
+
+
+
+  render() {
+    return (
+      <div>
+        <div className='box'>
+          <form action="">
+            <span className="text-center">Welcome to Valgrind</span>
+            <div className="input-container">
+              <input type="email" required="required" placeholder="User@gmail.com" value={this.state.user.name} onChange={this.handleNameChange(this)} />
+              <label>User:
           </label>
-          </div>
-          <div className="input-container">
-            <input type="password" required="required" placeholder="************" />
-            <label>Password:</label>
-          </div>
-          <ButtonGroup aria-label="Toolbar">
-            <Button variant="default me-md-2" className="btn" href='#/Portal'>Ingresar</Button>{''}
-            <Button variant="default me-md-2" className="btn" href='#/Register'>Registrar</Button>{''}
-          </ButtonGroup>
-        </form>
+            </div>
+            <div className="input-container">
+              <input type="password" placeholder="************" />
+              <label>Password:</label>
+            </div>
+            <ButtonGroup aria-label="Toolbar">
+              {/* <Button variant="default me-md-2" className="btn" href='#/Portal'>Ingresar</Button>{''} */}
+              <Button variant="default me-md-2" className="btn" >setDData</Button>{''}
+              {/* <Button variant="default me-md-2" className="btn" href='#/Register'>Registrar</Button>{''} */}
+              <Button variant="default me=md-2" className="btn" onClick={this.handleButtonClicked.bind(this)}> getData </Button> 
+            </ButtonGroup>
+          </form>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
-export default Welcome;
+export default Welcome; 
