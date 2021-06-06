166fbe82c262812a6b482dc131e6c53962220d2b
VictorP <A00827302@itesm.mx>
2021-06-05 19:29:20 -0500

    progres

---
+++ .vscode/frontEnd/src/Header/VerticalTabs.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import DashBoard from '@material-ui/icons/Dashboard'
+import VideogameAssetIcon from '@material-ui/icons/VideogameAsset';
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+    display: 'flex',
+    height: 224,
+  },
+  tabs: {
+    borderRight: `1px solid ${theme.palette.divider}`,
+  },
+}));
+
+export default function VerticalTabs(Props) {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(Props.tab);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Tabs
+        orientation="vertical"
+        variant="scrollable"
+        value={value}
+        onChange={handleChange}
+        aria-label="Vertical tabs example"
+        className={classes.tabs}
+      >
+        <Tab aria-current href='#/Portal' label="Home"/>
+        <Tab aria-current href='#/UserProfile' label="User"/>
+        <Tab label="Game"/>
+      </Tabs>
+      <TabPanel value={value} index={0}>
+        <DashBoard fontSize='large' />
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        <AccountBoxIcon fontSize='large' />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <VideogameAssetIcon fontSize='large' />
+      </TabPanel>
+    </div>
+  );
+}
---
+++ .vscode/frontEnd/src/Pages/Welcome.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import '../CSS/App.css';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+// TODO: Fix the error of missign "{" check this page for more info https://www.pluralsight.com/guides/extracting-input-from-a-form-with-reactjs
+
+
+class Welcome extends React.Component {
+  
+  constructor(props) {
+    super(props);
+
+    this.onChangeName = this.onChangeName.bind(this); 
+    this.state = { 
+      id: '', 
+      name: '', 
+      password: '', 
+      position: ''
+    }
+  }
+
+  onChangeName(e){ 
+    this.setState({name: e.target.value})
+  }
+  onChangePassword(e){
+    this.setState({password: e.target.value})
+  }
+
+  async getCredentials(){
+    const options = { 
+      method: "POST", 
+      headers: {"Content-Type":"application/json"}, 
+      body: JSON.stringify({name: this.state.name}),
+    }; 
+    const response = await fetch('login', options); 
+    const data = await response.json(); 
+    console.log("this is the data")
+    console.log(data); 
+
+    if(data.login){ 
+      sessionStorage.setItem('name', data.resultado.name); 
+      sessionStorage.setItem('id', data.resultado.id); 
+      sessionStorage.setItem('position', data.resultado.position); 
+      sessionStorage.setItem('manager', data.resultado.manager); 
+      sessionStorage.setItem('password', data.resultado.password);
+      window.open('#/Portal',"_self")
+    }
+    else{
+      alert("username or password is wrong")
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <div className='box'>
+          <form action="">
+            <span className="text-center">Welcome to Valgrind</span>
+            <div className="input-container">
+              <input type="email" required="required" placeholder="User@gmail.com" value={this.state.name} onChange={this.onChangeName.bind(this)} />
+              <label>User:</label>
+            </div>
+            <div className="input-container">
+              <input type="password" required='required' placeholder="************" value ={this.state.password} onChange={this.onChangePassword.bind(this)} />
+              <label>Password:</label>
+            </div>
+            <ButtonGroup aria-label="Toolbar">
+              <Button variant="default me-md-2" className="btn" onClick= {this.getCredentials.bind(this)}>Login</Button>{''}
+
+              <Button variant="default me-md-2" className="btn" href='#/Register'>Register</Button>{''}
+            </ButtonGroup>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+export default Welcome; 
